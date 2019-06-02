d7d160498905ea11478eb41e34629fdda1103ac7
YSTYLE <lxy5266@live.com>
2019-06-02 11:21:06 +0800

    cp service file --force

--- auto-update.sh
+++ auto-update.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -e
 if [[ ! -d "/usr/lib/systemd/system" ]]; then
-  cp ec-server-update.service /usr/lib/systemd/system
-  cp ec-server-update.timer /usr/lib/systemd/system
+  cp -f ec-server-update.service /usr/lib/systemd/system
+  cp -f ec-server-update.timer /usr/lib/systemd/system
 else
-  cp ec-server-update.service /lib/systemd/system/
-  cp ec-server-update.timer /lib/systemd/system/
+  cp -f ec-server-update.service /lib/systemd/system/
+  cp -f ec-server-update.timer /lib/systemd/system/
 fi
 
 systemctl daemon-reload
